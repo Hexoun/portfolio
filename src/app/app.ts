04eb15c67191d259854c1c7f18423cad03ba76dc
Hexoun <thierrymu10062005@gmail.com>
2026-03-10 15:55:05 +0100

    fix nav

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
-import { Accueil } from './accueil/accueil';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Accueil],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,41 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header implements OnInit {
+
+  words: string[] = [
+    "Étudiant en 2ème année",
+    "Développeur web",
+    "Passionné de programmation"
+  ];
+
+  displayedText = '';
+  wordIndex = 0;
+  charIndex = 0;
+  typing = true;
+
+  ngOnInit() {
+    this.typeEffect();
+  }
+
+  typeEffect() {
+    const currentWord = this.words[this.wordIndex];
+
+    if (this.typing) {
+      this.displayedText = currentWord.substring(0, this.charIndex++);
+      if (this.charIndex > currentWord.length) {
+        this.typing = false;
+        setTimeout(() => this.typeEffect(), 1500);
+        return;
+      }
+    } else {
+      this.displayedText = currentWord.substring(0, this.charIndex--);
+      if (this.charIndex === 0) {
+        this.typing = true;
+        this.wordIndex = (this.wordIndex + 1) % this.words.length;
+      }
+    }
+
+    setTimeout(() => this.typeEffect(), this.typing ? 100 : 50);
+  }
+}
